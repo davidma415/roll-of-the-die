9b3b6cd9a9d588c02429bac202056ea915a3d88e
David Ma <davidma415@gmail.com>
2018-07-18 15:50:10 -0400

    Added most likely finished

---
+++ most_likely.rb
@@ -0,0 +1,19 @@
+## make an empty permutation with keys of 2-12. then we will utilize a loop to iterate through 1-6 on each dice and add the total. if the total is equivalent to one of the hash keys, it will increase that hash value by 1
+
+permutations = {}
+(2..12).each do |x|
+  permutations[x] = 0
+end
+
+(1..6).each do |x|
+  (1..6).each do |y|
+    permutations[x+y] += 1
+  end
+end
+
+permutations.each do |k, v|
+  puts "The odds of #{k} coming up are #{((v/36.to_f)*100).round}%"
+end
+
+# This will find the value that matches the max value along with the k value of the highest percentage.
+permutations.each {|k, v| puts "#{k} is the most likely to come up with a #{((v/36.to_f)*100).round}% chance" if v == permutations.values.max }
