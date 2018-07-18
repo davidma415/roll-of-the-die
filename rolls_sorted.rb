71a25006a7b9657655115fe2b228411099451792
David Ma <davidma415@gmail.com>
2018-07-18 14:09:22 -0400

    Finished sorting 10 rolls

--- rolls_sorted.rb
+++ rolls_sorted.rb
@@ -4,8 +4,25 @@ def roll_die
   return result
 end
 
-counter = 1
-while counter <= 10
-  puts "The result of your roll is #{roll_die}"
-  counter += 1
+def sort_rolls
+  counter = 1
+  rolls = []
+  while counter <= 10
+    rolls.push("The result of your roll is #{roll_die}")
+    counter += 1
+  end
+  return rolls.sort!
 end
+
+def print_res(rolls)
+  rolls.each do |x|
+    puts x
+  end
+end
+
+def init
+  sorted_rolls = sort_rolls
+  print_res(sorted_rolls)
+end
+
+init
