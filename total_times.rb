4c5afa8b67d48dd73dd6f4557dbb4241ef956ff1
David Ma <davidma415@gmail.com>
2018-07-18 15:31:54 -0400

    Finished total times a value occurs

---
+++ total_times.rb
@@ -0,0 +1,16 @@
+## make an empty permutation with keys of 2-12. then we will utilize a loop to iterate through 1-6 on each dice and add the total. if the total is equivalent to one of the hash keys, it will increase that hash value by 1
+
+permutations = {}
+(2..12).each do |x|
+  permutations[x] = 0
+end
+
+(1..6).each do |x|
+  (1..6).each do |y|
+    permutations[x+y] += 1
+  end
+end
+
+permutations.each do |k, v|
+  puts "#{k} occurs #{v} times"
+end
